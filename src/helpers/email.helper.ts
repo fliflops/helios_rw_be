93d4457c0d09c90604c3972edb3d2230591c76a3
jklpi <jomar.furiscal@k-logistikus.com.ph>
2025-09-29 12:37:39 +0800

    update formatting and email subject

--- src/helpers/email.helper.ts
+++ src/helpers/email.helper.ts
@@ -4,12 +4,13 @@ interface EmailPassword {
     email: string;
     first_name: string;
     password: string;
+    subject?: string;
 }
 
-export default async function sendMail({ email, first_name, password}: EmailPassword) {
+export default async function sendMail({ email, first_name, password, subject = '[Helios: New Account]'}: EmailPassword) {
     return await transporter.sendMail({
         to: email,
-        subject: '[Helios: New Account]',
+        subject: subject,
         html: `<p>Dear ${first_name},</p>
             <p>Below are your credentials to access Helios:</p>
             <p>Helios Link: ''</p>
